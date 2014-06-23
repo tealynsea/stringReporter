c6930c15c0444c1630b945633b288d0ea1fa299d
Teri <tealynsea@hotmail.com>
2014-06-23 16:05:38 -0600

    exercise completed

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+var word = prompt("Enter any word");
+
+alert("Hello.  You entered the word " + word + ".");
+
+alert("There are " + word.length + " letters in your word.");
+
+alert("The third charachter is " +word.charAt(2) + ".");
+
+alert("The word in lowercase looks like this:  " + word.toLowerCase() + ".");
+
+alert("The word in uppercase looks like this:  " + word.toUpperCase() + ".");
+
+alert("I ride my " + word + " to work.");
+
+alert("The subword from the 2nd to the 4th character is:  " + word.substring(1,4) + ".");
